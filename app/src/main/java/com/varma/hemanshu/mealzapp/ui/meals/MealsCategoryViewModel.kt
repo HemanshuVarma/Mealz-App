70fa27d6c10ad78323ee7fd9d0a62cdf960f793e
Hemanshu Varma <hemanshuvarma916@gmail.com>
2021-12-31 21:12:34 +0530

    Update: Code optimization for ViewModel Coroutine

--- app/src/main/java/com/varma/hemanshu/mealzapp/ui/meals/MealsCategoryViewModel.kt
+++ app/src/main/java/com/varma/hemanshu/mealzapp/ui/meals/MealsCategoryViewModel.kt
@@ -3,33 +3,25 @@ package com.varma.hemanshu.mealzapp.ui.meals
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.varma.hemanshu.mealzapp.model.MealsRepository
 import com.varma.hemanshu.mealzapp.model.response.MealResponse
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MealsCategoryViewModel(private val repository: MealsRepository = MealsRepository()) :
     ViewModel() {
 
-    private val mealsJob = Job()
     val mealsState: MutableState<List<MealResponse>> = mutableStateOf(emptyList())
 
     init {
-        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
-        scope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             mealsState.value = getMeals()
         }
-
     }
 
     private suspend fun getMeals(): List<MealResponse> {
         return repository.getMeals().categories
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        mealsJob.cancel()
-    }
 }
