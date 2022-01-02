2e3c81a25ba5bc2787e96926ddf163338583b186
Hemanshu Varma <hemanshuvarma916@gmail.com>
2022-01-02 18:29:43 +0530

    Add: AppBar

--- app/src/main/java/com/varma/hemanshu/mealzapp/ui/MainActivity.kt
+++ app/src/main/java/com/varma/hemanshu/mealzapp/ui/MainActivity.kt
@@ -3,6 +3,10 @@ package com.varma.hemanshu.mealzapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import com.varma.hemanshu.mealzapp.ui.meals.MealsCategoryScreen
 import com.varma.hemanshu.mealzapp.ui.theme.MealzAppTheme
 
@@ -11,8 +15,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MealzAppTheme {
-                MealsCategoryScreen()
+                Scaffold(topBar = { AppBar() }) {
+                    MealsCategoryScreen()
+                }
             }
         }
     }
+}
+
+@Composable
+fun AppBar() {
+    TopAppBar(
+        title = {
+            Text("Meals App")
+        }
+    )
 }
